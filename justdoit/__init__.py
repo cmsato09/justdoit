73a8c482993e4889d8f6ea015cc15266a160df6d
cmsato09 <55464277+cmsato09@users.noreply.github.com>
2024-07-08 17:44:46 -0700

    Create justdoit/__init__.py file with message
    - just do it message

---
+++ justdoit/__init__.py
@@ -0,0 +1,26 @@
+def main():
+    just_do_it = """
+    DO IT.
+    Just DO IT.
+
+    Don't let your dreams be dreams.
+    Yesterday, you said tomorrow.
+    So JUST DO IT.
+    Make your dreams come true.
+    Just DO IT.
+
+    Some people dream of success, while you're gonna wake up and work HARD at it.
+    Nothing is impossible.
+    You should get to the point where anyone else would quit, and you're not gonna stop there.
+
+    NO, what are you waiting for?
+    DO IT!
+    Just DO IT!
+    Yes you can.
+    Just do it.
+
+    If you're tired of starting over, stop giving up.
+    """
+    print(just_do_it)
+
+main()
